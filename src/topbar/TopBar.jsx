497a29de933fb08045bfafa3966ff618a8a264f1
Mikolee <mikobai524@gmail.com>
2023-10-04 22:50:11 +0800

    upload icons

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,9 +3,29 @@ import "./topbar.css";
 export default function TopBar() {
   return (
     <div className="top">
-      <div className="topLeft">l</div>
-      <div className="topCenter">c</div>
-      <div className="topRight">r</div>
+      <div className="topLeft">
+        <i className="fa-brands fa-square-facebook"></i>
+        <i className="fa-brands fa-square-twitter"></i>
+        <i className="fa-brands fa-square-github"></i>
+        <i className="fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://z1.ax1x.com/2023/10/04/pPONoSx.jpg"
+          alt="topImg"
+        />
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 }
