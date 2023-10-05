6b76e474a417d5aaf620213629bee55edc389556
Mikolee <mikobai524@gmail.com>
2023-10-06 03:10:13 +0800

    set the posts component

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import "./post.css";
 
 export default function Post() {
-  return <div>Post</div>;
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src="https://z1.ax1x.com/2023/10/06/pPXBA3j.jpg"
+        alt="post img"
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+    </div>
+  );
 }
