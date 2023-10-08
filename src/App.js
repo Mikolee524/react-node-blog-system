c638f26bbf65e1c8a2d7d07dc2b14880ac0c4844
Mikolee <mikobai524@gmail.com>
2023-10-09 07:11:24 +0800

    change the single css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/home";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Home />
+      <Single />
     </>
   );
 }
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,42 @@
+import "./singlePost.css";
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      SinglePost
+      <div className="singlePostWrapper">
+        <img
+          src="https://z1.ax1x.com/2023/10/06/pPXBA3j.jpg"
+          alt="singlePostImg"
+          className="singlePostImg"
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <div className="singlePostEdit">
+            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-regular fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Autuor:<b>Miko</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, odit
+          voluptate neque consequuntur reprehenderit rem quae blanditiis optio
+          minus recusandae, omnis modi laudantium nobis voluptas et quo
+          adipisci, eius libero?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Explicabo quibusdam dolorum quas rerum totam quasi
+          unde recusandae ad consectetur dignissimos numquam, nisi animi
+          perferendis quaerat nostrum at itaque temporibus cupiditate! Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Veritatis sit
+          exercitationem animi soluta, impedit aspernatur totam assumenda non
+          ipsum quam nostrum aliquam provident odio commodi dolores distinctio
+          recusandae quasi. Et.
+        </p>
+      </div>
+    </div>
+  );
+}
