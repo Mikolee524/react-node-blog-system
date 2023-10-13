0db10069c430a19e59e9d82a2f19b7eea9a7c2a4
Michael Li <michael.li.work@outlook.com>
2023-10-13 21:39:44 +0800

    new: login page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -11,18 +11,20 @@ export default function Settings() {
         </div>
         <form className="settingsForm">
           <label>Profile Picture</label>
-          <div className="settingsPP">
+          <div className="settingsImg">
             <img src="https://z1.ax1x.com/2023/10/05/pPX1dSO.png" alt="" />
             <label htmlFor="fileInput">
               <i className="settingsPPIcon fa-regular fa-circle-user"></i>
             </label>
-            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <div className="settingsPP">
             <label>Username</label>
             <input type="text" placeholder="Safak" />
             <label>Email</label>
             <input type="email" placeholder="safak@gmail.com" />
             <label>Password</label>
-            <input type="password" />
+            <input type="password" placeholder="******" />
             <button className="settingsSubmit">Update</button>
           </div>
         </form>
