71df027152e234a9432152a225b4ea3460f6844b
Michael Li <michael.li.work@outlook.com>
2023-10-14 16:55:45 +0800

    add: add route to homepage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Post from "./components/post/Post";
+import Posts from "./components/posts/Posts";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
@@ -5,13 +7,26 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
