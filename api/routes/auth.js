efc5feb49f777f9ee1a4db52d9bf2b06c396a109
Michael Li <michael.li.work@outlook.com>
2023-10-19 02:19:05 +0800

    new: add the bcrypt in auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/Users");
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt();
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
