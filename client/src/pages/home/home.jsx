af4e20e1615e885a457bdefe635c6794703d305a
Michael Li <michael.li.work@outlook.com>
2023-10-23 00:25:37 +0800

    feature: fetch post data from backend

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,13 +2,26 @@ import "./home.css";
 import Header from "../../components/Header/Header";
 import Posts from "../../components/posts/Posts";
 import SideBar from "../../components/siderbar/SideBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <SideBar />
       </div>
     </>
