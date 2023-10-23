66ed9426635fdd243c4b679bc893552db08f023b
Michael Li <michael.li.work@outlook.com>
2023-10-23 12:16:53 +0800

    fix: fix the cat name correctly

--- api/models/Post.js
+++ api/models/Post.js
@@ -19,7 +19,7 @@ const PostSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    categroies: {
+    categories: {
       type: Array,
       required: false,
     },
