c59b2b9d410108624fbb0d2c2e40c5dd4237394e
Michael Li <michael.li.work@outlook.com>
2023-10-26 04:01:35 +0800

    fix: some spell wrong in code

--- client/src/context/Actions.js
+++ client/src/context/Actions.js
@@ -2,11 +2,11 @@ export const LoginStart = (userCredentials) => ({
   type: "LOGIN_START",
 });
 
-export const LoginSuccess = {
+export const LoginSuccess = (user) => ({
   type: "LOGIN_SUCESS",
   payload: user,
-};
+});
 
-export const LoginFaliure = {
-  type: "LOGIN_FALIURE",
-};
+export const LoginFaliure = () => ({
+  type: "LOGIN_FAILURE",
+});
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
-import "./login.css";
-import { useContext, useRef } from "react";
-import { Context } from "../../context/Context";
 import axios from "axios";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./login.css";
 
 export default function Login() {
   const userRef = useRef();
@@ -13,8 +13,8 @@ export default function Login() {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("auth/login", {
-        username: useRef.current.value,
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
         password: passwordRef.current.value,
       });
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
@@ -22,8 +22,7 @@ export default function Login() {
       dispatch({ type: "LOGIN_FAILURE" });
     }
   };
-  console.log("user:" + user);
-  console.log("isFetching:" + isFetching);
+  console.log(user);
 
   return (
     <div className="login">
