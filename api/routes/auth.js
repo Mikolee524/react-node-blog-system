dc6907458df7c09ce2194d00ec3a0dfa9ed2ee26
Michael Li <michael.li.work@outlook.com>
2023-11-08 23:17:40 +0800

    fix: auth method

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -23,16 +23,14 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    // if (!user) {
-    //   return res.status(400).json("Wrong credentials!");
-    // }
-    !user && res.status(400).json("Wrong credentials!");
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
+    }
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    // if (!validated) {
-    //   return res.status(400).json("Wrong credentials!");
-    // }
-    !validated && res.status(400).json("Wrong credentials!");
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
+    }
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
